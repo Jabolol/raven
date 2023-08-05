edfefc084151b278ae5d99a77954c144eb7bbbd6
Javier R <javier.rios-urbano@epitech.eu>
2023-08-05 16:46:32 +0200

    add: `404` page

---
+++ routes/_404.tsx
@@ -0,0 +1,20 @@
+export default function Error404() {
+  return (
+    <div className="dark:bg-black dark:text-white h-screen flex flex-col items-center justify-center">
+      <img
+        className="my-6 rounded-md"
+        src="/raven.png"
+        width="128"
+        height="128"
+        alt="the raven logo"
+      />
+      <h1 className="text-4xl font-bold">404 - Page not found</h1>
+      <p className="my-4">
+        The page you were looking for doesn't exist.
+      </p>
+      <a href="/" className="underline">
+        Go back home
+      </a>
+    </div>
+  );
+}
