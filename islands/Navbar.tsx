fe6c6cc81b3ca05f89224c2eec11a1ee17982ca3
Javier R <javier.rios-urbano@epitech.eu>
2023-08-05 22:45:27 +0200

    add: `reload` option

--- islands/Navbar.tsx
+++ islands/Navbar.tsx
@@ -4,6 +4,7 @@ import IconMoon from "icons/moon.tsx";
 import IconSun from "icons/sun.tsx";
 import IconBrandGithub from "icons/brand-github.tsx";
 import IconLogout from "icons/logout.tsx";
+import IconReload from "icons/reload.tsx";
 import { logout } from "../state/auth.ts";
 
 export default function Navbar({ isLogged }: { isLogged: boolean }) {
@@ -29,20 +30,29 @@ export default function Navbar({ isLogged }: { isLogged: boolean }) {
           <h1 className="text-lg font-semibold">Raven</h1>
         </div>
         <div className="flex items-center justify-center p-5 gap-3">
+          {isLogged && (
+            <IconReload
+              onClick={() => location.reload()}
+              className="cursor-pointer"
+            />
+          )}
           {isDarkMode
             ? <IconSun onClick={toggleDarkMode} className="cursor-pointer" />
             : <IconMoon onClick={toggleDarkMode} className="cursor-pointer" />}
-          {isLogged && <IconSearch />}
+          {isLogged && <IconSearch className="cursor-pointer" />}
           {isLogged
             ? (
               <img
                 src="/logo.svg"
                 alt="raven logo"
-                className={`w-8 h-8 rounded-full`}
+                className={`w-8 h-8 rounded-full cursor-pointer`}
               />
             )
             : (
-              <a href="https://github.com/Jabolol/raven">
+              <a
+                href="https://github.com/Jabolol/raven"
+                className="cursor-pointer"
+              >
                 <IconBrandGithub />
               </a>
             )}
