d76966d672ed8aa4a35298fe5609bb4942147747
Javier R <javier.rios-urbano@epitech.eu>
2023-08-06 18:10:34 +0200

    add: `self` route

---
+++ islands/Self.tsx
@@ -0,0 +1,23 @@
+import { useEffect, useState } from "preact/hooks";
+import { isLoggedIn, store } from "../state/auth.ts";
+import Navbar from "./Navbar.tsx";
+import Me from "./Me.tsx";
+
+export default function Self() {
+  const [isLogged, setIsLoggedIn] = useState<boolean>(false);
+
+  useEffect(() => {
+    if (!isLoggedIn.value) window.location.href = "/";
+    store.subscribe((v) => {
+      setIsLoggedIn(v.loggedIn);
+    });
+    setIsLoggedIn(isLoggedIn.value);
+  }, []);
+
+  return (
+    <main class="bg-white dark:bg-gray-800">
+      <Navbar isLogged={isLogged} />
+      <Me />
+    </main>
+  );
+}
