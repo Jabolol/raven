f0a6663acd72b00d801c97f4088ead8d445397fb
Javier R <javier.rios-urbano@epitech.eu>
2023-08-06 16:14:22 +0200

    add: `user` state

---
+++ state/user.ts
@@ -0,0 +1,27 @@
+import { computed, signal } from "@preact/signals";
+import { type FriendResp } from "../types.ts";
+import { getAuth, refresh } from "./auth.ts";
+
+export const store = signal<FriendResp | null>(null);
+
+export const isFriend = (s: FriendResp | null): s is FriendResp => s !== null;
+
+export const user = computed(() => store.value);
+
+export const fetchFriend = async (profile_id: string): Promise<void> => {
+  const resp = await fetch("/api/user", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      profile_id,
+      access_token: getAuth()?.access_token,
+    }),
+  });
+  if (!resp.ok) {
+    await refresh();
+    return await fetchFriend(profile_id);
+  }
+  store.value = await resp.json();
+};
