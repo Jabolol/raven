43f2908a9e4b5df1ddf7513731e89409338b3a52
Javier R <javier.rios-urbano@epitech.eu>
2023-08-06 18:11:30 +0200

    add: `friends` backend

---
+++ routes/api/friends.ts
@@ -0,0 +1,38 @@
+import { HandlerContext } from "$fresh/server.ts";
+
+export const handler = async (
+  req: Request,
+  _ctx: HandlerContext,
+): Promise<Response> => {
+  if (req.method !== "POST") {
+    return new Response(null, { status: 405 });
+  }
+
+  const { access_token } = await req.json() as {
+    access_token?: string;
+  };
+
+  if (!access_token) {
+    return new Response(null, { status: 400 });
+  }
+
+  const meResponse = await fetch(
+    `https://mobile.bereal.com/api/relationships/friends`,
+    {
+      headers: {
+        authorization: `Bearer ${access_token}`,
+      },
+    },
+  );
+
+  if (!meResponse.ok) {
+    return new Response(null, { status: 401 });
+  }
+
+  return new Response(
+    JSON.stringify(
+      await meResponse.json(),
+    ),
+    { status: 200 },
+  );
+};
