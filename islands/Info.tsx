11ec809dc4f244ca2eef3ce3118af960a6103105
Javier R <javier.rios-urbano@epitech.eu>
2023-08-06 16:20:53 +0200

    fix: `localStorage` exception

--- islands/Info.tsx
+++ islands/Info.tsx
@@ -3,9 +3,11 @@ import { fetchFriend } from "../state/user.ts";
 import { isLoggedIn } from "../state/auth.ts";
 import Navbar from "./Navbar.tsx";
 import User from "./User.tsx";
+import { IS_BROWSER } from "$fresh/runtime.ts";
 
 export default function Info({ id }: { id: string }) {
   useEffect(() => {
+    if (!IS_BROWSER) return;
     if (!isLoggedIn.value) window.location.href = "/";
     fetchFriend(id);
   }, []);
