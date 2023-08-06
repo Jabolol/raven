eebb8ce4364b36367ba0016d90c4116e42a017a6
Javier R <javier.rios-urbano@epitech.eu>
2023-08-06 18:10:27 +0200

    add: `user`profile picture

--- islands/Navbar.tsx
+++ islands/Navbar.tsx
@@ -5,13 +5,16 @@ import IconBrandGithub from "icons/brand-github.tsx";
 import IconLogout from "icons/logout.tsx";
 import IconReload from "icons/reload.tsx";
 import IconArrowBigLeft from "icons/arrow-big-left.tsx";
+import IconUser from "icons/user.tsx";
 import { logout } from "../state/auth.ts";
+import { fetchMe, me } from "../state/me.ts";
 
 export default function Navbar({ isLogged }: { isLogged: boolean }) {
   const [isDarkMode, setIsDarkMode] = useState<boolean>(false);
 
   useEffect(() => {
     setIsDarkMode(document.body.classList.contains("dark"));
+    fetchMe();
   }, []);
 
   const toggleDarkMode = () => {
@@ -44,13 +47,17 @@ export default function Navbar({ isLogged }: { isLogged: boolean }) {
             ? <IconSun onClick={toggleDarkMode} className="cursor-pointer" />
             : <IconMoon onClick={toggleDarkMode} className="cursor-pointer" />}
           {isLogged
-            ? (
-              <img
-                src="/logo.svg"
-                alt="raven logo"
-                className={`w-8 h-8 rounded-full cursor-pointer`}
-              />
-            )
+            ? me.value === null
+              ? <IconUser className="w-6 h-6 rounded-full cursor-pointer" />
+              : (
+                <a href="/me">
+                  <img
+                    src={me.value.profilePicture.url}
+                    alt={me.value.username}
+                    className={`w-6 h-6 rounded-full cursor-pointer`}
+                  />
+                </a>
+              )
             : (
               <a
                 href="https://github.com/Jabolol/raven"
