f4e8c342a545a3277ce432aa53f01779d1656c80
Javier R <javier.rios-urbano@epitech.eu>
2023-08-08 13:20:29 +0200

    fix: `empty` feed

--- islands/Feed.tsx
+++ islands/Feed.tsx
@@ -3,6 +3,7 @@ import { feed, fetchFeed } from "../state/feed.ts";
 import Post from "./Post.tsx";
 import Spinner from "../components/Spinner.tsx";
 import Footer from "../components/Footer.tsx";
+import IconBoxOff from "icons/box-off.tsx";
 
 export default function Feed() {
   useEffect(() => {
@@ -11,11 +12,23 @@ export default function Feed() {
 
   return (
     <div className="flex flex-col items-center justify-center h-full">
-      {feed.value === null ? <Spinner /> : (
-        <div className="pb-8">
-          {feed.value.friendsPosts.reverse().map((post) => <Post {...post} />)}
-        </div>
-      )}
+      {feed.value === null
+        ? <Spinner />
+        : feed.value.friendsPosts.length === 0
+        ? (
+          <div className="flex justify-center items-center h-screen">
+            <IconBoxOff className="dark:text-gray-300 text-gray-800 text-center w-16 h-16" />
+          </div>
+        )
+        : (
+          <div className="pb-8">
+            {feed.value.friendsPosts.reverse().map((post) => (
+              <Post
+                {...post}
+              />
+            ))}
+          </div>
+        )}
       <Footer />
     </div>
   );
