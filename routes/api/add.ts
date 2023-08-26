8be4ae0b82fe838e8710f346aea5e303d000d4c5
Javier R <javier.rios-urbano@epitech.eu>
2023-08-26 19:06:38 +0200

    add: `request` friend endpoint

---
+++ routes/api/add.ts
@@ -0,0 +1,59 @@
+import { HandlerContext } from "$fresh/server.ts";
+
+export const handler = async (
+  req: Request,
+  _ctx: HandlerContext,
+): Promise<Response> => {
+  if (req.method !== "POST") {
+    return new Response(null, { status: 405 });
+  }
+
+  const { access_token, userId } = await req.json() as {
+    access_token?: string;
+    userId?: string;
+  };
+
+  if (!access_token || !userId) {
+    return new Response(null, { status: 400 });
+  }
+
+  const meResponse = await fetch(
+    `https://mobile.bereal.com/api/relationships/friend-requests`,
+    {
+      headers: {
+        authorization: `Bearer ${access_token}`,
+      },
+      method: "POST",
+      body: JSON.stringify({
+        userId,
+        source: "search",
+      }),
+    },
+  );
+
+  if (!meResponse.ok) {
+    return new Response(
+      JSON.stringify(
+        await meResponse.json(),
+      ),
+      {
+        status: 400,
+        headers: {
+          "Content-Type": "application/json",
+        },
+      },
+    );
+  }
+
+  return new Response(
+    JSON.stringify(
+      await meResponse.json(),
+    ),
+    {
+      status: 200,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    },
+  );
+};
