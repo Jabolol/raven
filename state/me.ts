f7ac27f49f0859acc204830b84e62a1993e0b73d
Javier R <javier.rios-urbano@epitech.eu>
2023-08-26 19:07:45 +0200

    add: `state` fix

--- state/me.ts
+++ state/me.ts
@@ -1,6 +1,6 @@
 import { computed, signal } from "@preact/signals";
-import { type MeResp } from "../types.ts";
-import { getAuth, getSession, refresh } from "./auth.ts";
+import { type MeResp } from "~/types.ts";
+import { getSession, refresh } from "~/state/auth.ts";
 
 export const store = signal<MeResp | null>(null);
 
@@ -9,14 +9,17 @@ export const isMe = (s: MeResp | null): s is MeResp => s !== null;
 export const me = computed(() => store.value);
 
 export const fetchMe = async (): Promise<void> => {
-  getSession();
+  const auth = await getSession();
+  if (!auth) {
+    return;
+  }
   const resp = await fetch("/api/me", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
     },
     body: JSON.stringify({
-      access_token: getAuth()?.access_token,
+      access_token: auth.access_token,
     }),
   });
   if (!resp.ok) {
