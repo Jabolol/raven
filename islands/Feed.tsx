e54ad7c47479749985199a71d788380d20b916a5
Javier R <javier.rios-urbano@epitech.eu>
2023-08-26 18:59:44 +0200

    fix: `clean` code

--- islands/Feed.tsx
+++ islands/Feed.tsx
@@ -1,8 +1,7 @@
 import { useEffect } from "preact/hooks";
-import { feed, fetchFeed } from "../state/feed.ts";
-import Post from "./Post.tsx";
-import Spinner from "../components/Spinner.tsx";
-import Footer from "../components/Footer.tsx";
+import { feed, fetchFeed } from "~/state/feed.ts";
+import Post from "~/islands/Post.tsx";
+import Spinner from "~/components/Spinner.tsx";
 import IconBoxOff from "icons/box-off.tsx";
 
 export default function Feed() {
@@ -10,26 +9,29 @@ export default function Feed() {
     fetchFeed();
   }, []);
 
+  if (feed.value === null) {
+    return <Spinner />;
+  }
+
+  if (feed.value.friendsPosts.length === 0) {
+    return (
+      <div className="flex justify-center items-center h-screen">
+        <IconBoxOff className="dark:text-gray-300 text-gray-800 text-center w-16 h-16" />
+      </div>
+    );
+  }
+
   return (
     <div className="flex flex-col items-center justify-center h-full">
-      {feed.value === null
-        ? <Spinner />
-        : feed.value.friendsPosts.length === 0
-        ? (
-          <div className="flex justify-center items-center h-screen">
-            <IconBoxOff className="dark:text-gray-300 text-gray-800 text-center w-16 h-16" />
-          </div>
-        )
-        : (
-          <div className="pb-8">
-            {feed.value.friendsPosts.reverse().map((post) => (
-              <Post
-                {...post}
-              />
-            ))}
-          </div>
-        )}
-      <Footer />
+      <div className="pb-4">
+        {feed.value.friendsPosts.reverse().map((post) => (
+          <Post
+            user={post.user}
+            region={post.region}
+            posts={post.posts}
+          />
+        ))}
+      </div>
     </div>
   );
 }
