f6e7fdd6458367741a3852a153a6b6cb3d7e0377
Javier R <javier.rios-urbano@epitech.eu>
2023-08-26 19:02:12 +0200

    add: `request` friend

--- islands/User.tsx
+++ islands/User.tsx
@@ -1,75 +1,107 @@
 import { useEffect } from "preact/hooks";
-import { fetchFriend, user } from "../state/user.ts";
-import { isLoggedIn } from "../state/auth.ts";
-import Spinner from "../components/Spinner.tsx";
-
-const formatDate = (dateString: string) =>
-  new Date(dateString).toLocaleDateString(undefined, {
-    year: "numeric",
-    month: "short",
-    day: "numeric",
-  });
+import { fetchFriend, user } from "~/state/user.ts";
+import { getSession } from "~/state/auth.ts";
+import Spinner from "~/components/Spinner.tsx";
 
 export default function User({ id }: { id: string }) {
   useEffect(() => {
-    if (!isLoggedIn.value) window.location.href = "/";
     fetchFriend(id);
   }, []);
 
   const friend = user.value;
 
-  return friend === null ? <Spinner /> : (
-    <div
-      className={`profile-page ${
-        friend.isRealPeople ? "real-people" : ""
-      } dark:bg-gray-800 dark:text-white min-h-screen max-w-md mx-auto pb-4`}
-    >
-      <div
-        className={`profile-header ${
-          friend.isRealPeople ? "real-people-header" : ""
-        } relative`}
-      >
+  if (friend === null) {
+    return <Spinner />;
+  }
+
+  return (
+    <div className="profile-page dark:bg-gray-800 dark:text-white p-4 min-h-screen max-w-md mx-auto pb-4">
+      <div className="relative">
         <img
           src={(friend.profilePicture ?? { url: "/raven.png" }).url}
           alt={friend.username}
-          className="profile-picture w-full h-auto"
+          className="profile-picture w-full h-auto rounded-md"
+          onError={(d) => {
+            (d.target as HTMLImageElement).src = "/raven.png";
+          }}
         />
       </div>
-      <div className="profile-details ml-4 mt-4">
+      <div className="profile-details mt-4">
         <h1 className="username text-3xl font-semibold">
           @{friend.username}
         </h1>
         <p className="location text-lg text-gray-300">{friend.location}</p>
         <p className="bio text-xl mt-2">{friend.biography}</p>
-        <p className="text-gray-400 mt-6">
-          {friend.relationship.status === null
-            ? "You are not friends"
-            : `Friends since ${formatDate(friend.relationship.friendedAt)}`}
-        </p>
-      </div>
-      <div className="bg-gray-100 dark:bg-gray-900 p-4 m-4 rounded-md">
-        <h2 className="text-lg font-semibold mb-2">
-          Common Friends ({friend.relationship.commonFriends.total})
-        </h2>
-        <div className="grid grid-cols-2 gap-4">
-          {friend.relationship.commonFriends.sample.map((commonFriend) => (
-            <a
-              href={`/user/${commonFriend.id}`}
-              key={commonFriend.id}
-              className="p-2 rounded-lg bg-white dark:bg-gray-800"
+        {friend.relationship.status
+          ? (
+            <p className="text-gray-400 mt-6">
+              Friends since {new Date(friend.relationship.friendedAt)
+                .toLocaleDateString(
+                  undefined,
+                  {
+                    year: "numeric",
+                    month: "short",
+                    day: "numeric",
+                  },
+                )}
+            </p>
+          )
+          : (
+            <button
+              className="opacity-50 cursor-not-allowed w-full my-4 py-3 rounded-md bg-black text-white border-white dark:bg-white dark:border-black dark:text-black"
+              disabled
+              onClick={async (evt) => {
+                const auth = await getSession();
+                if (auth === null) {
+                  return;
+                }
+                const response = await fetch("/api/add", {
+                  method: "POST",
+                  body: JSON.stringify({
+                    userId: id,
+                    access_token: auth.access_token,
+                  }),
+                });
+                if (!response.ok) {
+                  console.error(response.status, await response.json());
+                  return;
+                }
+                (evt.target as HTMLButtonElement).disabled = true;
+              }}
             >
-              <img
-                src={(commonFriend.profilePicture ?? { url: "/raven.png" }).url}
-                alt={commonFriend.username}
-                className="w-full h-auto rounded-md"
-              />
-              <p className="text-xs mt-1">
-                @{commonFriend.username}
-              </p>
-            </a>
-          ))}
-        </div>
+              Add Friend
+            </button>
+          )}
       </div>
+      {friend.relationship.commonFriends.total > 0 && (
+        <div className="bg-gray-100 dark:bg-gray-900 p-4 my-4 rounded-md">
+          <h2 className="text-lg font-semibold mb-2">
+            Common Friends ({friend.relationship.commonFriends.total})
+          </h2>
+          <div className="grid grid-cols-2 gap-4">
+            {friend.relationship.commonFriends.sample.map((commonFriend) => (
+              <a
+                href={`/user/${commonFriend.id}`}
+                key={commonFriend.id}
+                className="p-2 rounded-lg bg-white dark:bg-gray-800"
+              >
+                <img
+                  src={(commonFriend.profilePicture ?? { url: "/raven.png" })
+                    .url}
+                  alt={commonFriend.username}
+                  className="w-full h-auto rounded-md"
+                  onError={(d) => {
+                    (d.target as HTMLImageElement).src = "/raven.png";
+                  }}
+                />
+                <p className="text-xs mt-1">
+                  @{commonFriend.username}
+                </p>
+              </a>
+            ))}
+          </div>
+        </div>
+      )}
     </div>
   );
 }
