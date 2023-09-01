cee35348a8e154208a5a1e241dcc4fd3ad8fdc54
Javier R <javier.rios-urbano@epitech.eu>
2023-09-01 11:40:53 +0200

    fix: `trailing` comma

--- main.ts
+++ main.ts
@@ -8,6 +8,6 @@ import "$std/dotenv/load.ts";
 
 import { start } from "$fresh/server.ts";
 import manifest from "~/fresh.gen.ts";
-import config from "~/fresh.config.ts"
+import config from "~/fresh.config.ts";
 
 await start(manifest, config);
